827599db53a2d6abacc981f54a6756d99bf6c6b7
KyleChutjian <kyle.chutjian@gmail.com>
2020-10-15 23:17:44 -0400

    testt

--- src/Screens/InstructionsScreen.java
+++ src/Screens/InstructionsScreen.java
@@ -32,17 +32,7 @@ public class InstructionsScreen extends Screen {
     public void initialize() {
         // setup graphics on screen (background map, spritefont text)
         //background = new TitleScreenMap();
-        background.setAdjustCamera(false);
-        
-        //Goals text
-        goalLabel = new SpriteFont("   GOAL", 60, 180, "Comic Sans", 23, Color.gray);
-        goalExplanationLabel = new SpriteFont("Reach the end while avoiding enemies and hazards.", 60, 200, "Comic Sans", 18, Color.gray);
-        
-        //Controls text
-        controlsLabel = new SpriteFont("   CONTROLS", 60, 250, "Comic Sans", 23, Color.gray);
-        controlsExplanationLabel1 = new SpriteFont("Left Arrow & Right Arrow Keys  : Move left and right", 60, 270, "Comic Sans", 18, Color.gray);
-        controlsExplanationLabel2 = new SpriteFont("Up Arrow Key                            : Jump", 60, 290, "Comic Sans", 18, Color.gray);
-        controlsExplanationLabel3 = new SpriteFont("Down Arrow Key                       : Duck", 60, 310, "Comic Sans", 18, Color.gray);
+
         controlsExplanationLabel4 = new SpriteFont("SPACE Bar                               : Interact", 60, 330, "Comic Sans", 18, Color.gray);
         controlsExplanationLabel5 = new SpriteFont("' P '                                            : Pause", 60, 350, "Comic Sans", 18, Color.gray);
         
